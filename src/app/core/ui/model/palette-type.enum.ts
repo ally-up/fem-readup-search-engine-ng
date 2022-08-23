e0a4e14aa0ddf89fef97e594989b4922cfc5e4c3
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-08-23 21:56:52 +0200

    chore: add ui module #1

---
+++ src/app/core/ui/model/palette-type.enum.ts
@@ -0,0 +1,25 @@
+/**
+ * Enum containing color palettes
+ */
+export enum PaletteType {
+  RED,
+  PINK,
+  PURPLE,
+  DEEP_PURPLE,
+  INDIGO,
+  BLUE,
+  LIGHT_BLUE,
+  CYAN,
+  TEAL,
+  GREEN,
+  LIGHT_GREEN,
+  LIME,
+  YELLOW,
+  AMBER,
+  ORANGE,
+  DEEP_ORANGE,
+  BROWN,
+  GREY,
+
+  FEM_READUP_RED
+}
