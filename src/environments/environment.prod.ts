fca046ff8e2c655444a4521f7e8284736efa7a55
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-08-26 15:27:06 +0200

    fix: prod budgets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,14 @@
 export const environment = {
   production: true,
   app_title: "FEM READUP",
-  theme: "fem-readup-red-theme"
+  theme: "fem-readup-red-theme",
+
+  firebase: {
+    apiKey: "AIzaSyAlx9J9srxQKBiF3I_7P8hv23Jfm-PoPVs",
+    authDomain: "fem-readup.firebaseapp.com",
+    projectId: "fem-readup",
+    storageBucket: "fem-readup.appspot.com",
+    messagingSenderId: "56848903230",
+    appId: "1:56848903230:web:f075e6e2668cf713ea356f"
+  }
 };
