a419bf42c23d66463e0186cce4502768e01dcc92
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-08-27 11:30:07 +0200

    chore: turn app into PWA #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpClientModule} from "@angular/common/http";
 import {FIREBASE_OPTIONS} from "@angular/fire/compat";
 import {environment} from "../environments/environment";
+import {ServiceWorkerModule} from "@angular/service-worker";
 
 @NgModule({
   declarations: [
@@ -21,6 +22,12 @@ import {environment} from "../environments/environment";
     UiModule,
 
     AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {provide: FIREBASE_OPTIONS, useValue: environment.firebase}
