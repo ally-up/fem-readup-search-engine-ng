53c9310cef8d6c9531c559d5428cc0b5da993163
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-08-28 15:49:23 +0200

    chore: format event dates

--- src/app/pages/overview/components/event-list-item/event-list-item.component.ts
+++ src/app/pages/overview/components/event-list-item/event-list-item.component.ts
@@ -14,7 +14,62 @@ export class EventListItemComponent implements OnInit {
   /** Event to be displayed */
   @Input() event: Event = new Event();
 
-  ngOnInit(): void {
-    console.log(`FOO ${this.event.id} ${this.event.image}`);
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init lifecycle phase
+   */
+  ngOnInit() {
+  }
+
+  //
+  // Helpers
+  //
+
+  formatDate(identifier: string, startValue: string, endValue: string) {
+    const startDate = new Date(startValue.replace("Z", ""));
+    const endDate = new Date(endValue.replace("Z", ""));
+
+    const containsMinutes = startValue.includes("T");
+    const spansMultipleDays = startDate.getDate() != endDate.getDate() || startDate.getMonth() != endDate.getMonth();
+
+    let result = startDate.toLocaleString('de-de', {weekday: 'long'}) + ", " +
+      (startDate.getDate()) + ". " +
+      startDate.toLocaleString('de-de', {month: 'short'}) + " ";
+
+    if (!spansMultipleDays) {
+      result += startDate.getFullYear() + " ";
+    }
+
+    if (containsMinutes) {
+      result += this.toTwoDigits(startDate.getHours()) + ":" + this.toTwoDigits(startDate.getMinutes());
+    }
+
+    if (containsMinutes && !spansMultipleDays) {
+      result += " - " + this.toTwoDigits(endDate.getHours()) + ":" + this.toTwoDigits(endDate.getMinutes()) + " Uhr ";
+    }
+
+    if (spansMultipleDays) {
+      result += " - " + endDate.toLocaleString('de-de', {weekday: 'long'}) + ", " +
+        (endDate.getDate()) + ". " +
+        endDate.toLocaleString('de-de', {month: 'short'}) + " " +
+        endDate.getFullYear() + " ";
+    }
+
+    if (containsMinutes && spansMultipleDays) {
+      result += this.toTwoDigits(endDate.getHours()) + ":" + this.toTwoDigits(endDate.getMinutes()) + " Uhr ";
+    }
+
+    return result;
+  }
+
+  toTwoDigits(value: number) {
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value;
+    }
   }
 }
