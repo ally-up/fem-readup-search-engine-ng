0552352cbfda2e0429c69453038bb5d32431ef9a
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-08-29 23:41:53 +0200

    chore: update title style in details page #4

--- src/app/pages/details/components/details-toolbar/details-toolbar.component.ts
+++ src/app/pages/details/components/details-toolbar/details-toolbar.component.ts
@@ -1,4 +1,8 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {filter, takeUntil} from "rxjs/operators";
+import {MediaService} from "../../../../core/ui/services/media.service";
+import {Media} from "../../../../core/ui/model/media.enum";
+import {Subject} from "rxjs";
 
 /**
  * Displays toolbar for details page
@@ -8,12 +12,38 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
   templateUrl: './details-toolbar.component.html',
   styleUrls: ['./details-toolbar.component.scss']
 })
-export class DetailsToolbarComponent {
+export class DetailsToolbarComponent implements OnInit {
 
   /** Title */
   @Input() title = '';
   /** Event emitter indicating menu item being clicked */
   @Output() menuItemEventEmitter = new EventEmitter<string>();
+
+  /** Enum of media types */
+  public mediaType = Media;
+  /** Current media */
+  public media: Media = Media.UNDEFINED;
+
+  /** Helper subject used to finish other subscriptions */
+  private unsubscribeSubject = new Subject();
+
+  /**
+   * Constructor
+   * @param mediaService media service
+   */
+  constructor(private mediaService: MediaService) {
+  }
+
+  //
+  // Lifecycle hooks
+  //
+
+  /**
+   * Handles on-init lifecycle phase
+   */
+  ngOnInit() {
+    this.initializeMedia();
+  }
 
   //
   // Actions
@@ -24,5 +54,19 @@ export class DetailsToolbarComponent {
    */
   onBackClicked() {
     this.menuItemEventEmitter.emit('back');
+  }
+
+  /**
+   * Initializes media
+   */
+  initializeMedia() {
+    this.mediaService.mediaSubject.pipe(
+      takeUntil(this.unsubscribeSubject),
+      filter(value => value != null)
+    ).subscribe(media => {
+      this.media = media;
+    });
+
+    this.mediaService.fetchMedia();
   }
 }
