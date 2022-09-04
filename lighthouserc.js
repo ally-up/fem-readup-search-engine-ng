ac8691ade9bc72046c40e6063c6db477523fbc2d
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-09-04 23:11:00 +0200

    chore: specify static dist dir #10

---
+++ lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: './dist/fem-readup-search-engine-ng',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
