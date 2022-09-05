c1c4235c3ae6018ffbcdd20da6acd7186f9ffc40
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-09-05 07:47:22 +0200

    chore(lighthouse): enable preloading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 
 /* List of available routes */
 const routes: Routes = [
@@ -12,7 +12,7 @@ const routes: Routes = [
 ;
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {enableTracing: false})],
+  imports: [RouterModule.forRoot(routes, {enableTracing: false, preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
