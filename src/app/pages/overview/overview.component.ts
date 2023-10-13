29e15584f11fa85b1d7bb84f50d5c3b78c490f60
karopp <karoline@familie-rabe.de>
2023-10-13 09:40:17 +0200

    fix: make search-panel scrollable #22

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
 import {Subject} from 'rxjs';
 import {MatIconRegistry} from '@angular/material/icon';
 import {DomSanitizer} from '@angular/platform-browser';
@@ -290,6 +290,7 @@ return environment.defaultImageUrl
     switch (menuItem) {
       case 'filter': {
         this.searchPanelState = this.searchPanelState === 'panel-open' ? 'panel-closed' : 'panel-open';
+        window.scrollTo(0,0); 
         break;
       }
       case 'filter-reset': {
