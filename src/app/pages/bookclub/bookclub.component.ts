eab42bf7d3b2c610b65b5cc2fe97e22af1352029
karopp <karoline@familie-rabe.de>
2023-11-10 15:14:42 +0100

    chore: add readinglist

--- src/app/pages/bookclub/bookclub.component.ts
+++ src/app/pages/bookclub/bookclub.component.ts
@@ -4,6 +4,19 @@ import { environment } from 'src/environments/environment';
 import {MatExpansionModule} from '@angular/material/expansion';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import booksJson from 'src/assets/bookclub/books.json';
+
+interface Book {
+  title: String;
+  author: String;
+  month: Number;
+  year: Number;
+}
+
+interface ReadingListYear {
+  year: Number;
+  books: Book[];
+}
 
 
 @Component({
@@ -18,6 +31,7 @@ export class BookclubComponent implements OnInit {
   /** App title */
   appTitle = environment.app_title;
   panelOpenState = false;
+  readingLists: ReadingListYear[] = booksJson;
 
 
   constructor(private router: Router, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer, ) {
@@ -28,6 +42,7 @@ export class BookclubComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.readingLists = this.readingLists.sort((a,b) => (a.year > b.year ? -1 : 1));
   }
 
   onBackClicked() {
